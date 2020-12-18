41fd5236944f16ac49171984918fa5519642041d
szlhj <szlhj09@gmail.com>
2020-12-18 10:04:06 +0900

    주석 변경
    주석 변경

--- src/shop/beggar/admin/action/AdminAction.java
+++ src/shop/beggar/admin/action/AdminAction.java
@@ -6,7 +6,20 @@ import javax.servlet.http.HttpServletResponse;
 import shop.beggar.common.Action;
 import shop.beggar.common.ActionForward;
 
-
+/**
+ * @PackageName		: shop.beggar.admin.action
+ * @FileName		: AdminAction.java
+ * @Since			: 2020. 12. 7.
+ * @Author			: HJLee
+ * @Description		: 
+ * =====================================================================================
+ * 								   Modification History
+ * =====================================================================================
+ * Date				Author				Note
+ * -------------------------------------------------------------------------------------
+ * 2020. 12. 7.		HJLee				최초 작성
+ *
+ */
 public class AdminAction implements Action{
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
