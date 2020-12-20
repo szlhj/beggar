45e1ed431fd783cb027e08789734558fdb49cc9e
szlhj <szlhj09@gmail.com>
2020-12-20 23:20:53 +0900

    관리자 메인 명 수정
    관리자 메인 명 수정

--- src/shop/beggar/admin/action/AdminAction.java
+++ src/shop/beggar/admin/action/AdminAction.java
@@ -1,5 +1,15 @@
 package shop.beggar.admin.action;
 
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import shop.beggar.admin.service.AdminService;
+import shop.beggar.common.Action;
+import shop.beggar.common.ActionForward;
+
 /**
  * @PackageName		: shop.beggar.admin.action
  * @FileName		: AdminAction.java
@@ -14,6 +24,18 @@ package shop.beggar.admin.action;
  * 2020. 12. 7.		HJLee				최초 작성
  *
  */
-public class AdminAction {
-
+public class AdminAction implements Action{
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		AdminService svc = new AdminService();
+		int count = svc.supperAdminInfo();
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("adminSupperCount", count);
+		
+		ActionForward forward = new ActionForward();
+		forward.setPath("/views/admin/adminMain.jsp");
+		return forward;
+	}
 }
